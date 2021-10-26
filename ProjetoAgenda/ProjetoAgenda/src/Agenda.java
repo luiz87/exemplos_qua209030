dcf6cab0e0b05ab4ae3e17cefeae20e6ece344ff
luiz-felipe <luizfelipe.freire@gmail.com>
2021-10-26 20:08:28 -0300

    add comentários no cod

--- ProjetoAgenda/ProjetoAgenda/src/Agenda.java
+++ ProjetoAgenda/ProjetoAgenda/src/Agenda.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Agenda {
     private ArrayList<Contato> lsContato;
 
+    // Método contrutor da classe, ele vai ser usado no momento que o objeto da classe Agenda for criado.
     public Agenda() {
         lsContato = new ArrayList<>();
     }
@@ -25,8 +26,11 @@ public class Agenda {
 
     // ver lista de contato
     public String verLista() {
+        // variavel feita para concater todo o resultado e ser retornada no final
         String texto = "";
+        // variavel feita para contar os elemtos do ArrayList e servir como indice dos itens da lista
         int i = 0;
+        // laço de repetição feito para mostrar tooodos os itens do ArrayList
         for (Contato c : lsContato) {
             texto += "[" + i + "] Nome:" + c.getNome() + " Fone:" + c.getTelefone() + "\n";
             i++;
